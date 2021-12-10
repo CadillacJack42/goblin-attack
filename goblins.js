b592151eb87b9740979c91bc7900a42d6f360f96
James Keicher <jamesdkeicher@gmail.com>
2021-12-10 13:05:49 -0800

    Saving progress. generate/display/kil goblins functional

---
+++ goblins.js
@@ -0,0 +1,62 @@
+export const goblinArray = [
+    {
+        name: 'George',
+        hp: Math.ceil(Math.random() * 4),
+        image: './assets/enemies/enemy-1.jpeg',
+        id: undefined
+    }, 
+    {
+        name: 'Fred',
+        hp: Math.ceil(Math.random() * 4),
+        image: './assets/enemies/enemy-2.jpeg',
+        id: undefined
+    },
+    {
+        name: 'Billy',
+        hp: Math.ceil(Math.random() * 4),
+        image: './assets/enemies/enemy-3.jpeg',
+        id: undefined
+    },
+    {
+        name: 'Bob',
+        hp: Math.ceil(Math.random() * 4),
+        image: './assets/enemies/enemy-4.jpeg',
+        id: undefined
+    },
+    {
+        name: 'Courage',
+        hp: Math.ceil(Math.random() * 4),
+        image: './assets/enemies/enemy-5.jpeg',
+        id: undefined
+    },
+    {
+        name: 'Thug life',
+        hp: Math.ceil(Math.random() * 4),
+        image: './assets/enemies/enemy-6.jpeg',
+        id: undefined
+    },
+    {
+        name: 'Jerk face',
+        hp: Math.ceil(Math.random() * 4),
+        image: './assets/enemies/enemy-7.jpeg',
+        id: undefined
+    },
+    {
+        name: 'Punch head',
+        hp: Math.ceil(Math.random() * 4),
+        image: './assets/enemies/enemy-8.jpeg',
+        id: undefined
+    },
+    {
+        name: 'Sir sure',
+        hp: Math.ceil(Math.random() * 4),
+        image: './assets/enemies/enemy-9.jpeg',
+        id: undefined
+    },
+    {
+        name: 'Big Daddy',
+        hp: Math.ceil(Math.random() * 4),
+        image: './assets/enemies/enemy-10.jpeg',
+        id: undefined
+    }
+];
